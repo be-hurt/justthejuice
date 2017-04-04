c36619b82e7deda6b9c9814fbf5d33203e8f185e
Britt <bee.hurt@gmail.com>
2017-04-04 17:33:40 -0400

    Added (empty) pages, set up routing for pages, got basic layout template working

--- justthejuice.js
+++ justthejuice.js
@@ -9,3 +9,29 @@ app.engine('handlebars', handlesbars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+//404 catch all handler (middleware)
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+});
