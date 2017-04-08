16c4d5959b6063dfa4508ed8e69b58c8969af98b
be-hurt <bee.hurt@gmail.com>
2017-04-08 11:23:47 -0400

    Delete credentials.js

--- credentials.js
+++
@@ -1,8 +0,0 @@
-mongo : {
-    development: {
-        connectionString: 'mongodb://be-hurt:juicenebula@ds060369.mlab.com:60369/jtj'
-    },
-        production: {
-            connectionString: 'mongodb://be-hurt:juicenebula@ds060369.mlab.com:60369/jtj'
-        }
-}
