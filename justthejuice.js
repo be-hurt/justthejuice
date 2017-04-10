c3e298713954b86106c8e6dc1ea030f76dc3e831
Britt <bee.hurt@gmail.com>
2017-04-10 18:15:39 -0400

    Added new-recipe page, form, routing and ability to upload new recipes to the database

--- justthejuice.js
+++ justthejuice.js
@@ -6,10 +6,12 @@ app.set('ip', process.env.IP || 'localhost');
 app.disable('x-powered-by');
 
 //mongodb setup
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var credentials = require('./credentials.js');
 var Recipe = require('./models/recipes.js');
+var RecipeListener = require('./models/recipeListener.js');
 
 require('./lib/recipesData.js');
 
@@ -34,9 +36,26 @@ app.set('view engine', 'handlebars');
 //Startup
 app.use(express.static(__dirname + '/public'));
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}));
+
+app.use(require('body-parser').urlencoded({extended : true}));
+
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
+    next();
+});
+
+//flash messages
+app.use(function(req, res, next) {
+    //if there's a flash message, transfer if to the context, then clear it
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
     next();
 });
 
@@ -56,7 +75,6 @@ switch (app.get('env')) {
     default:
         throw new Error('Unknown execution environment: ' + app.get('env'));
 };
-
 
 //Routes
 app.get('/', function (req, res) {
@@ -83,7 +101,6 @@ app.get('/', function (req, res) {
         }
         var random_recipe = get_random();
         res.render('home', random_recipe);
-        console.log('HERE', random_recipe);
     });
 });
 
@@ -93,6 +110,43 @@ app.get('/about', function (req, res) {
 
 app.get('/my-account', function (req, res) {
     res.render('my-account');
+});
+
+app.get('/new-recipe', function (req, res) {
+    res.render('new-recipe', {recipe_id: req.query.recipe_id});
+});
+
+app.post('/new-recipe', function(req, res){
+    Recipe.update(
+        {recipe_name: req.body.recipe_name,
+        user: req.body.user,
+        category: req.body.category,
+        rating: 0,
+        description: req.body.description,
+        ingredients: req.body.ingredients,
+        steps: req.body.steps,
+        notes: req.body.notes,
+        image: ' '},
+        {$push: {recipe_id: req.body.recipe_id}},
+        {upsert: true},
+        function(err){
+        if(err) {
+            console.error(err.stack);
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Oops!',
+                message: 'There was an error processing your request.'
+            };
+            return res.redirect(303, '/recipes');
+        }
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'Your recipe has been added to the database.'
+        };
+        return res.redirect(303, '/recipes');
+        }
+    );
 });
 
 app.get('/recipes', function (req, res) {
--- models/recipeListener.js
+++ models/recipeListener.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var recipeSchema = mongoose.Schema({
+var recipeListenerSchema = mongoose.Schema({
     recipe_name: {type: String, required: true},
     user: String,
     category: {type: String, required: true},
@@ -13,5 +13,5 @@ var recipeSchema = mongoose.Schema({
     recipe_id: [String]
 });
 
-var Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+var recipeListener = mongoose.model('recipeListener', recipeListenerSchema);
+module.exports = recipeListener;
